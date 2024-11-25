90a38fc7989a732a25c8f3cc6aa58c0bdb80ce1e
loller-skates <jltejera20@gmail.com>
2024-11-25 17:40:50 -0500

    thanks

---
+++ script.js
@@ -0,0 +1,137 @@
+function methodOne() {
+    alert('LMAOOOOOOO LOSER jk i love you');
+}
+
+function methodTwo() {
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+    alert(
+        "Your system is heavily damaged by Two viruses! \n\n" +
+        "We detect that your Apple iPad is 28.1% DAMAGED because of Two harmful viruses from recent adult sites. \n\n" +
+        "Soon it will damage your phone's SIM card and will corrupt your contacts, photos, data, applications, etc. \n\n" +
+        "If you do not remove the virus now, it will cause severe damage to your phone. Here's what you NEED to do (step by step): \n\n" +
+        "Step 1: Tap the button and install App for free! \n\n" +
+        "Step 2: Open the App to speed up and fix your browser now! \n\n" +
+        "WARNING! \n\n" +
+        "This Apple iPad is infected with viruses and your browser is seriously damaged. You need to remove viruses and make corrections immediately. \n\n" +
+        "It is necessary to remove and fix now. \n\n" +
+        "Don't close this window. \n\n" +
+        "If you leave, you will be at risk."
+    );
+}
+
+function methodThree(){
+    const url = "https://open.spotify.com/playlist/5I26YyYS8ZVrGIh4xjXHkW?si=329f1c1052fc4d4b&pt=eb8e974e3a1f2dfedb3b45683cd444c2";
+    const width = 600;
+    const height = 800;
+    const left = (window.screen.width - width) / 2;
+    const top = (window.screen.height - height) / 2;
+
+    window.open(
+        url,
+        "Spotify Playlist",
+        `width=${width},height=${height},top=${top},left=${left},resizable=yes,scrollbars=yes`
+    );
+}
